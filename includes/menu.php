0adfe35ae88b1b958668f05400cc6b648316688c
João Vitor <joaooviitorr@hotmail.com>
2019-09-18 23:15:13 -0300

    Pasta de arquivos usados frequentemente na aplicacao

---
+++ includes/menu.php
@@ -0,0 +1,34 @@
+<nav class="navbar  navbar-light  bg-dark">
+        <div class="text-light" >
+            <script src="../js/data.js"></script>
+        </div>
+
+        <ul class="nav justify-content-end ">
+          <li class="nav-item">
+            <a class="text-light nav-link " href="../index.php" title="Voltar a pagina Principal"> Inicio </a>
+          </li>
+            <li class="nav-item">
+            <a class="text-light nav-link " href="../read/listarEmpresa.php" title="Listar informações das empresas"> Empresas </a>
+          </li>
+          <li class="nav-item">
+            <a class="text-light nav-link " href="../read/listarFuncionario.php" title="Listar informações dos funcionarios"> Funcionarios </a>
+          </li>
+          <li class="nav-item">
+            <a class="text-light nav-link " href="../read/listarVeiculo.php" title="Listar informações dos veiculos"> Veiculos </a>
+          </li>
+        <li class="nav-item">
+          <a class="text-light nav-link " href="../relatorio/selecionarCampos.php" title="Pagina de Relatorios" > Relatorio </a>
+        </li>
+        <!--<li class="nav-item">
+          <a class="text-light nav-link " href="../login/sair.php" title="Encerrar Sessão" logout-confirm="Tem certeza que deseja excluir o item selecionado?"> Sair </a>
+        </li>-->
+        </ul>
+        <div class="text-light">
+            <label ID="Clock">00:00:00</label>
+            <script src="../js/hora.js"></script>
+            
+        </div>
+    </nav>
+
+    
+    
